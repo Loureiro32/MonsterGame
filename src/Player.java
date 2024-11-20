5f89b3e1df574625514bdcbd55a666b81b894e5d
Mindera <mindera@C02Z689TLVCF-ME00547.local>
2024-11-20 17:49:02 +0000

    removed unused code

--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,6 @@ public class Player {
     protected int maxCards = 4;
     protected Monster[] monsters = new Monster[maxCards];
     protected String name;
-    protected int cardCounter = 0;
 
 
     public Player(String name) {
@@ -20,7 +19,6 @@ public class Player {
                 currentPlayerCards[i] = Game.dealCard();
             }
         }
-
     }
 
 }
